4542fde04cab6e60b0c6402b1b06f9425c865816
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-19 15:38:40 +0200

    Adding default Id property name to LoggedEntityAttribute

--- src/HodStudio.EfDiffLog/Model/LoggedEntityAttribute.cs
+++ src/HodStudio.EfDiffLog/Model/LoggedEntityAttribute.cs
@@ -5,6 +5,15 @@ namespace HodStudio.EfDiffLog.Model
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class LoggedEntityAttribute : Attribute
     {
+        public const string DefaultIdPropertyName = "Id";
+
+        /// <summary>
+        /// Defines that the entity will be logged when using the EfDiffLog.
+        /// It will use the default id property name ("Id").
+        /// If your class has a different id property name, use the overload, where you can provide the Id property name.
+        /// </summary>
+        public LoggedEntityAttribute() : this(DefaultIdPropertyName) { }
+
         /// <summary>
         /// Defines that the entity will be logged when using the EfDiffLog.
         /// </summary>
