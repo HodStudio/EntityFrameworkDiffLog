47b56b0b614fb0cf4d3b9a8b5593336d21aded31
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-19 23:06:23 +0200

    Core: add-migration LogEntries

--- src/Examples/DotNetCore/Migrations/20191019210353_AddingLogEntry.Designer.cs
+++ src/Examples/DotNetCore/Migrations/20191019210353_AddingLogEntry.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20191019210353_AddingLogEntry")]
+    partial class AddingLogEntry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
