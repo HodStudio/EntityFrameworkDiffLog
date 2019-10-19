743b674ffbed55358ada0f185a5e13212d771de6
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-19 18:24:29 +0200

    Lib: possibility to override table and schema name

--- src/HodStudio.EfDiffLog/Repository/LoggingDbContext.cs
+++ src/HodStudio.EfDiffLog/Repository/LoggingDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using HodStudio.EfDiffLog.Model;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,9 +12,25 @@ namespace HodStudio.EfDiffLog.Repository
 {
     public class LoggingDbContext : DbContext
     {
+        protected string LogEntriesTableName { get; set; } = "LogEntries";
+        protected string LogEntriesSchemaName { get; set; } = "dbo";
         public DbSet<LogEntry> LogEntries { get; set; }
 
         public string UserId { get; set; }
+
+#if NETSTANDARD
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LogEntry>().ToTable(LogEntriesTableName);
+            base.OnModelCreating(modelBuilder);
+        }
+#else
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LogEntry>().ToTable(LogEntriesTableName, LogEntriesSchemaName);
+            base.OnModelCreating(modelBuilder);
+        }
+#endif
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
