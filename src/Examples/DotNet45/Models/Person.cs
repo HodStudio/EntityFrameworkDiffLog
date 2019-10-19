23028813ab72d43fe496c325e1cd4a76058cd2ba
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-19 19:34:20 +0200

    DotNet45: Include the `LoggedEntity` on Person
    In the case, the Person is a base class that Students and Instructors inherits. We have just to put the attribute on it.

--- src/Examples/DotNet45/Models/Person.cs
+++ src/Examples/DotNet45/Models/Person.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using HodStudio.EfDiffLog.Model;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
+    [LoggedEntity(nameof(ID))]
     public abstract class Person
     {
         public int ID { get; set; }
