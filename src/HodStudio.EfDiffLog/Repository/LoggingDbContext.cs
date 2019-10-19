de7ec160502e42f1ec8d7c39e1f59514187e4411
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-19 17:42:25 +0200

    Library: removing methods that will not be used

--- src/HodStudio.EfDiffLog/Repository/LoggingDbContext.cs
+++ src/HodStudio.EfDiffLog/Repository/LoggingDbContext.cs
@@ -7,57 +7,12 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 #else
 using System.Data.Entity;
-using System.Data.Entity.Core.Objects;
-using System.Data.Entity.Infrastructure;
-using System.Data.Common;
 #endif
 
 namespace HodStudio.EfDiffLog.Repository
 {
     public class LoggingDbContext : DbContext
     {
-        public LoggingDbContext(params Assembly[] assemblies)
-        {
-            InitializeAssemblies(assemblies);
-        }
-
-#if NETSTANDARD
-        public LoggingDbContext(DbContextOptions<LoggingDbContext> options, params Assembly[] assemblies) : base(options)
-        {
-            InitializeAssemblies(assemblies);
-        }
-#else
-        public LoggingDbContext(string nameOrConnectionString, params Assembly[] assemblies) : base(nameOrConnectionString)
-        {
-            InitializeAssemblies(assemblies);
-        }
-
-        public LoggingDbContext(string nameOrConnectionString, DbCompiledModel model, params Assembly[] assemblies) : base(nameOrConnectionString, model)
-        {
-            InitializeAssemblies(assemblies);
-        }
-
-        public LoggingDbContext(DbConnection existingConnection, bool contextOwnsConnection, params Assembly[] assemblies) : base(existingConnection, contextOwnsConnection)
-        {
-            InitializeAssemblies(assemblies);
-        }
-
-        public LoggingDbContext(ObjectContext objectContext, bool dbContextOwnsObjectContext, params Assembly[] assemblies) : base(objectContext, dbContextOwnsObjectContext)
-        {
-            InitializeAssemblies(assemblies);
-        }
-
-        public LoggingDbContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection, params Assembly[] assemblies) : base(existingConnection, model, contextOwnsConnection)
-        {
-            InitializeAssemblies(assemblies);
-        }
-
-        protected LoggingDbContext(DbCompiledModel model, params Assembly[] assemblies) : base(model)
-        {
-            InitializeAssemblies(assemblies);
-        }
-#endif
-
         public DbSet<LogEntry> LogEntries { get; set; }
 
         public string UserId { get; set; }
@@ -66,12 +21,6 @@ namespace HodStudio.EfDiffLog.Repository
         {
             await this.LogChanges(UserId);
             return await base.SaveChangesAsync(cancellationToken);
-        }
-
-        private void InitializeAssemblies(params Assembly[] assemblies)
-        {
-            foreach (var assembly in assemblies)
-                this.InitializeIdColumnNames(assembly);
         }
     }
 }
