f3893cd6f9a6191e64d387dad294641060ec79a1
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-19 16:49:02 +0200

    Adding the ContosoUniversity_DotNet45

---
+++ src/Examples/DotNet45/Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="jquery-1.10.2.js" />
+/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="respond.js" />
