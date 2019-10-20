cdf4d6e50f7a8a339880fd0f0e71522873cee8cf
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-20 16:20:30 +0200

    Adds Examples back

---
+++ src/Examples/DotNetCore/Migrations/20191019210353_AddingLogEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class AddingLogEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "LogEntries",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    EntityName = table.Column<string>(nullable: true),
+                    EntityId = table.Column<string>(nullable: true),
+                    Operation = table.Column<string>(nullable: true),
+                    LogDateTime = table.Column<DateTime>(nullable: false),
+                    ValuesChanges = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogEntries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogEntries",
+                schema: "dbo");
+        }
+    }
+}
