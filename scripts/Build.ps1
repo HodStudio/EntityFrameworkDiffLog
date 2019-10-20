1fd0c5b63c0bed7e8603e461206d3ff57ae07145
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-20 02:58:12 +0200

    Configures the build script

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -57,7 +57,7 @@ exec { & dotnet restore }
 Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = ".{0:D2}" -f [convert]::ToInt32($revision, 10)
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 # exec { & dotnet test .\test\MediatR.Tests -c Release }
 
