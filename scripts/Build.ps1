0c10c61b4e1cd6bce2aed78e4a5fa2023ebfcf07
Cussa Mitre <cussa.mitre@gmail.com>
2019-10-20 03:31:02 +0200

    Configures buildNumber

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -31,7 +31,7 @@ exec { & dotnet restore }
 exec { & dotnet build $projectPath -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+$revision = "beta-{0:D2}" -f [convert]::ToInt32($revision, 10)
 
 # exec { & dotnet test .\test\HodStudio.EfDiffLog.Tests -c Release }
 
